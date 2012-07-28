ba5f76c7ff8386fe6a981f8a1e5d26257be32abf
Mani Tadayon & Luis De Leon <bowsersenior@gmail.com>
2012-07-27 20:46:35 -0700

    Fix step definition for sslyze to define installation procedure

--- lib/gauntlt/cucumber/sslyze_steps.rb
+++ lib/gauntlt/cucumber/sslyze_steps.rb
@@ -1,16 +1,35 @@
 Given /^sslyze is installed$/ do
-  steps %{
-    When I run `which sslyze.py`
-    Then the output should contain:
-    """
-    sslyze.py
-    """
-  } 
+  begin
+    steps %{
+      When I run `which sslyze`
+      Then the output should contain:
+      """
+      sslyze
+      """
+    }
+  rescue
+    msg = <<-EOS
+    sslyze.py not installed or sslyze alias not set!
+
+    1. Download sslyze from: https://github.com/iSECPartners/sslyze
+    2. Create a file named 'sslyze' with the following content:
+
+       #! /usr/bin/env bash
+       python /path/to/sslyze.py
+
+    3. Make sure the file is in your path:
+
+       $ which sslyze
+
+
+EOS
+    raise msg
+  end
 end
 
 When /^I run sslyze against the hostname$/ do
     steps %{
-        When I run `sslyze.py --regular \"#{hostname}\":443`
+        When I run `sslyze --regular \"#{hostname}\":443`
     }
 end
 
