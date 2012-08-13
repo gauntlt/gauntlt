0eab91a04d0ea6b1770ba1d731bbd124355773d4
Alexander Tamoykin <at@zestcash.com>
2012-08-13 01:56:08 -0700

    Multiple attacks with -a option https://github.com/thegauntlet/gauntlt/issues/26

--- lib/gauntlt/attack.rb
+++ lib/gauntlt/attack.rb
@@ -6,15 +6,15 @@ module Gauntlt
     class NotFound < Exception; end
     class ExecutionFailed < Exception; end
 
-    attr_accessor :name, :opts, :attack_file
+    attr_accessor :name, :opts, :attack_files
 
     def initialize(name, opts={})
-      if opts[:attack_file] && File.exists?( opts[:attack_file] )
+      if opts[:attack_files] && files_exist?(opts[:attack_files])
         self.name = name
         self.opts = opts
-        self.attack_file = opts[:attack_file]
+        self.attack_files = opts[:attack_files]
       else
-        raise NotFound.new("No '#{opts[:attack_file]}' attack found")
+        raise NotFound.new("No '#{opts[:attack_files]}' attack found")
       end
     end
 
@@ -27,13 +27,21 @@ module Gauntlt
     end
 
     def run
-      cli = Cucumber::Cli::Main.new([self.attack_file, '--strict', '--require', self.attacks_dir])
+      cucumber_options = self.attack_files + ['--strict', '--require', self.attacks_dir]
+      cli = Cucumber::Cli::Main.new(cucumber_options)
 
       if cli.execute! # cucumber failed, returning true
         raise ExecutionFailed.new("Bad or undefined attack!")
       else            # cucumber executed successfully, returning false
         true
       end
+    end
+
+    private
+
+    def files_exist? attacks
+      existance = attacks.map {|a| File.exists? a}
+      !existance.include?(false)
     end
   end
 end
