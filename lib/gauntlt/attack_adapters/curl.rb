d3d4312641016bfc324d64a0d6c781f19d4920ca
Mani Tadayon <bowsersenior@gmail.com>
2012-10-26 19:47:44 -0500

    Remove obsolete step definitions

--- lib/gauntlt/attack_adapters/curl.rb
+++ lib/gauntlt/attack_adapters/curl.rb
@@ -2,19 +2,6 @@ When /^"curl" is installed$/ do
   ensure_cli_installed("curl")
 end
 
-When /^I launch a "curl" attack$/ do
-  # curl custom output
-  # from:
-  #   http://beerpla.net/2010/06/10/how-to-display-just-the-http-response-code-in-cli-curl/
-  #
-  # for more output variables, see:
-  #   http://man.he.net/man1/curl
-  @raw_response = `curl --silent --output /dev/null --write-out "%{http_code}" "#{hostname}"`
-  @response = {
-    :code => @raw_response
-  }
-end
-
 When /^I launch a "curl" attack with:$/ do |command|
   command.gsub!('<hostname>', hostname)
   run command
@@ -22,10 +9,6 @@ end
 
 Then /^the response code should be "(.*?)"$/ do |http_code|
   @response[:code].should == http_code
-end
-
-When /^I launch a "cookies" attack$/ do
-  set_cookies( cookies_for(hostname) )
 end
 
 Then /^the following cookies should be received:$/ do |table|
