0720b576a5a014fa3e692d61eca0a9f98d4bb2b9
James Wickett <wickett@gmail.com>
2013-07-31 21:35:15 -0500

    changing the way we reference attacks

--- lib/gauntlt/attack_adapters/nmap.rb
+++ lib/gauntlt/attack_adapters/nmap.rb
@@ -1,4 +1,6 @@
 # TODO: figure out if there's a way to namespace these step definitions
+#
+@attack_steps = {'nmap-fast' => 'nmap -F <hostname>'}
 
 When /^"nmap" is installed$/ do
   ensure_cli_installed("nmap")
@@ -6,4 +8,10 @@ end
 
 When /^I launch an "nmap" attack with:$/ do |command|
   run_with_profile command
+end
+
+When /^I launch a "nmap-(.*?)" attack$/ do |step_name|_
+  step = 'nmap-' + step_name
+  nmap_attack = @attack_steps[step] || raise("Unknown attack step #{step}")
+  run_with_profile nmap_attack
 end
