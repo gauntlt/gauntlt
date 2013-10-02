fa3099d86b629a024c18b55afc164b3c2e049fb1
Brandon Perry <bperry.volatile@gmail.com>
2013-10-02 13:23:16 -0700

    add a way to auth against a web application before testing with arachni

--- lib/gauntlt/attack_adapters/gauntlt.rb
+++ lib/gauntlt/attack_adapters/gauntlt.rb
@@ -1,4 +1,5 @@
 require 'nokogiri'
+require 'net/http'
 
 Given /^the following environment variables:$/ do |table|
   table.hashes.each do |hsh|
@@ -22,4 +23,52 @@ When /^the file "(.*?)" should not contain XML:$/ do |filename, css_selectors|
   css_selectors.hashes.each do |row|
     assert_xml_does_not_include(filename, row['css'])
   end
+end
+
+And /^gauntlt successfully authenticates to the application/ do
+  init = gauntlt_profile["initial_url"]
+  uri = URI.parse(init)
+  req = Net::HTTP::Get.new(uri.path)
+  res = Net::HTTP.start(uri.host, uri.port) do |http|
+      http.request(req)
+  end
+
+  cookie = res['Set-Cookie']
+  token = ''
+  if gauntlt_profile["get_rails_auth_token"] == "true"
+    res.body.each_line do |line|
+      next if line !~ /<input name="authenticity_token"/
+
+      ix = line.index("<input name=\"authenticity_token\"")
+      tmp = line[ix..res.body.length-1]
+      tmp = tmp[0..tmp.index('/>')+1]
+
+      token = Nokogiri::XML::Document.parse(tmp)
+      token = token.root.attributes["value"]
+      break
+    end
+  end
+
+  params = (token == '' ? '' : 'authenticity_token=' + token)
+  gauntlt_profile.keys.each do |key|
+    next if key !~ /login-/
+
+    k = key.sub('login-', '')
+    params << (params == '' ? '' : '&') + k + '=' + gauntlt_profile[key]
+  end
+
+  headers = {
+    'Cookie' => cookie
+  }
+
+  req = Net::HTTP::Post.new(gauntlt_profile['login_uri'], headers)
+  req.body = params
+
+  resp = Net::HTTP.new(uri.host, uri.port).start do |http| 
+    http.request(req)
+  end
+
+  raise 'Data did not match regex' if resp['Location'] !~ /#{gauntlt_profile['login_expected_redirect']}/
+
+  add_to_profile('cookie_string', cookie)
 end
