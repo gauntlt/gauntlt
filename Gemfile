03f5d5968205caa9a05c44a442f87a74b4c73e85
James Wickett <wickett@gmail.com>
2014-06-17 10:49:39 -0500

    moving to new rspec syntax

--- Gemfile
+++ Gemfile
@@ -2,12 +2,5 @@ source 'https://rubygems.org'
 
 gemspec
 
-# to get railsgoat working
+# to get gruyere target working
 gem 'service_manager'
-
-gem 'debugger', :platform => :mri
-gem 'ruby-debug-base', :platform => :jruby
-
-gem 'sqlite3', :platform => :mri
-gem 'jdbc-sqlite3', :platform => :jruby
-
--- lib/gauntlt/attack_adapters/curl.rb
+++ lib/gauntlt/attack_adapters/curl.rb
@@ -12,7 +12,7 @@ Then /^the following cookies should be received:$/ do |table|
 
   names = table.hashes.map{|h| h['name'] }
   names.each do |name|
-    cookies.any?{|s| s =~ /^#{name}/}.should be_true
+    expect(cookies.any?{|s| s =~ /^#{name}/}).to eq(true)
     # TODO: check other values in table
   end
 end
