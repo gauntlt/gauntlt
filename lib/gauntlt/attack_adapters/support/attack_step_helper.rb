ea6b78ed6d4014b6191630cf23e2a5fd2052bd31
Chris G <cgotcu@gmail.com>
2016-02-02 20:49:19 -0500

    fixed valiable issue

--- lib/gauntlt/attack_adapters/support/attack_step_helper.rb
+++ lib/gauntlt/attack_adapters/support/attack_step_helper.rb
@@ -15,9 +15,10 @@ module Gauntlt
             alias_versioned << "-"
             alias_versioned << version[0]
         end
+	print @attack_steps
         if @attack_steps.include?(alias_versioned)
             @attack_steps[alias_versioned]
-        elsif
+        elsif  
             @attack_steps[alias_name]
         else
             raise("Unknown attack alias #{alias_name}, try gauntlt --list_attack_aliases")
