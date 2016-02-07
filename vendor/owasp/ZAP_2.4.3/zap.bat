c779b613b481da660b12c4e908b760c4034a372a
Chris G <cgotcu@gmail.com>
2016-02-07 16:55:29 -0500

    ??

--- vendor/owasp/ZAP_2.4.3/zap.bat
+++
@@ -1,7 +0,0 @@
-if exist "%HOMEPATH%\OWASP ZAP\.ZAP_JVM.properties" (
-	set /p jvmopts=< "%HOMEPATH%\OWASP ZAP\.ZAP_JVM.properties"
-) else (
-	set jvmopts=-Xmx512m
-)
-
-java %jvmopts% -jar zap-2.4.3.jar %*
--- vendor/owasp/zap_wrapper.rb
+++ vendor/owasp/zap_wrapper.rb
@@ -3,8 +3,8 @@ require 'owasp_zap'
 include OwaspZap
 
 target = ARGV[0]
-host = ARGV[1] not nil ? ARGV[1] : 'localhost'
-port = ARGV[2] not nil ? ARGV[2] : '8080'
+host = ARGV[1].nil? ? 'localhost' : ARGV[1]
+port = ARGV[2].nil? ? '8080' : ARGV[2]
 
 z = Zap.new :target=>target, 
     :zap=>"./ZAP_2.4.3/zap.sh",
