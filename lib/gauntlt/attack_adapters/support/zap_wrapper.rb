dbfb04763f96548afe3f7b8c811f6175a3d142fd
Chris Gotcu <cgotcu@gmail.com>
2016-02-08 14:45:32 -0500

    ugh

--- lib/gauntlt/attack_adapters/support/zap_wrapper.rb
+++ lib/gauntlt/attack_adapters/support/zap_wrapper.rb
@@ -2,47 +2,39 @@ require 'owasp_zap'
 
 include OwaspZap
 
-target = ARGV[0]
-host = ARGV[1] not nil ? ARGV[1] : 'localhost'
-port = ARGV[2] not nil ? ARGV[2] : '8080'
+module Gauntlt
+    module Support
+        module ZapHelper
+            def launch_attack()
+                target = ARGV[0]
+                host = ARGV[1].nil? ? 'localhost' : ARGV[1]
+                port = ARGV[2].nil? ? '8080' : ARGV[2]
 
-name = 'owasp_zap'
-g = Gem::Specification.find_by_name(name)
-path = File.join(g.full_gem_path, 'vendor/owasp/zap_wrapper.sh')
-puts path
+                z = Zap.new :target=>target,
+                            :zap=> '~/ZAP/zap.sh',
+                            :output=>'~/zap-logfile.txt', # it will log the stdout log from Zap Proxy to a file
+                            :daemon => true,
+                            :base => 'http://' + host + ':8090'
 
-z = Zap.new :target=>target, 
-    :zap=> path,
-    :output=>'logfile.txt', # it will log the stdout log from Zap Proxy to a file
-    :daemon => true,
-    :base => 'http://' + host + ':8090'
-    
-z.start :daemon => true, :port => port, :host => host
+                z.start :daemon => true, :port => port, :host => host
 
-while !z.running?
-    sleep(5)
+                while !z.running?
+                    sleep(5)
+                end
+
+                spidey = z.spider
+                spidey.start
+
+                sleep(2)
+                if spidey.running?
+                    print spidey.status
+                end
+
+                z.ascan.start
+                print z.alerts.view
+                z.shutdown
+            end
+        end
+    end
 end
 
-spidey = z.spider
-spidey.start
-
-sleep(2)
-if spidey.running?
-    print spidey.status
-end
-
-z.ascan.start # to run active scan
-#alert = z.alerts
-print z.alerts.view
-#z.alerts.view # you can specify one format JSON, XML or HTML.. default JSON.
-z.shutdown # stop the proxy
-#
-# # to disable a specific test
-# to_be_disabled = JSON.load(z.policy.all)["policies"].select { |p| p["name"] == "Information gathering" }.first
-#
-# unless to_be_disabled.nil?
-#     z.scanner.disable([to_be_disabled["id"]])
-#     end
-#
-#     # to print the XML report
-#     z.xml_report
